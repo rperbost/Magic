975d34710e23bfd3c649fca92518ae068c702487
totony <totony@lj67.(none)>
2012-12-10 13:10:21 +0100

    refactoring drawable card

--- src/ihm/ClickableCard.java
+++ src/ihm/ClickableCard.java
@@ -12,100 +12,56 @@ import javax.swing.JPanel;
 import player.Player;
 
 @SuppressWarnings("serial")
-public class ClickableCard extends JLabel{
+public class ClickableCard extends DrawableCard{
 
-	static int cardWidth = 100;
-	static int cardHeight = 142;
-	static int boxWidth = cardWidth+15;
-	static int boxHeight = cardHeight+15;
+	private int boxWidth = CARD_WIDTH+15;
+	private int boxHeight = CARD_HEIGHT+15;
 	
 	private Player player;
 	int cardId;
-	private ZoomedCard zoomedCard;	
 	
 	public ClickableCard(Player p,int cardId, JPanel parent){
-		super(new ImageIcon(p.getCard(cardId).getLilImageLink()));
-		
+		super(p.getCard(cardId),parent);
 		
 		int col = cardId%5;
 		int row = cardId/5;
 		
-		zoomedCard = new ZoomedCard(p.getCard(cardId));	
-		parent.add(zoomedCard);
-		
-		parent.setComponentZOrder(zoomedCard, 0);
-		
 		int x = col*boxWidth+15;
 		int y = row*boxHeight+45;
 		
-		this.setBounds(x, y, cardWidth, cardHeight);
+		this.setLocation(x, y);
 		
-		final ClickableCard me = this;
-		this.addMouseListener(new MouseListener() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-				me.player.setSelectedCard(me.cardId);
-				System.out.println(me.cardId);
-				
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-				zoomedCard.setVisible(true);
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				setCursor(Cursor.getDefaultCursor());
-				zoomedCard.setVisible(false);
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				
-				
-			}
-
-			
-			
-		});
-		
-		this.addMouseMotionListener(new MouseMotionListener() {
-			
-			@Override
-			public void mouseMoved(MouseEvent e) {
-				int x = e.getPoint().x +me.getX() +1;
-				int y = e.getPoint().y +me.getY() +1;
-
-				if(x > 300){
-					x -=  ZoomedCard.WIDTH -2;
-				}
-				if(y > 273){
-					y -=  ZoomedCard.HEIGHT -2;
-				}
-				
-				zoomedCard.setLocation(x,y);
-			}
-			
-			@Override
-			public void mouseDragged(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
 		this.player = p;
-		this.cardId = cardId;
-		this.setVisible(true);
-		this.setSize(100, 142);
+		this.cardId = cardId;		
+		
+	}
+	
+	public void mouseEntered(MouseEvent e) {
+		super.mouseEntered(e);
+		setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 	}
 
+	public void mouseExited(MouseEvent e) {
+		super.mouseExited(e);
+		setCursor(Cursor.getDefaultCursor());
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		super.mouseClicked(e);
+		this.player.setSelectedCard(this.cardId);
+	}
+	public void mouseMoved(MouseEvent e) {
+		int x = e.getPoint().x +this.getX() +1;
+		int y = e.getPoint().y +this.getY() +1;
+
+		if(x > 300){
+			x -=  ZoomedCard.WIDTH -2;
+		}
+		if(y > 273){
+			y -=  ZoomedCard.HEIGHT -2;
+		}
+		
+		zoomedCard.setLocation(x,y);
+	}
 }
+
---
+++ src/ihm/DrawableCard.java
@@ -0,0 +1,69 @@
+package ihm;
+
+import java.awt.Cursor;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import binder.Card;
+
+@SuppressWarnings("serial")
+public abstract class DrawableCard  extends JLabel implements MouseListener,MouseMotionListener{
+	
+	static final int CARD_WIDTH = 100;
+	static final int CARD_HEIGHT = 142;
+	
+	protected ZoomedCard zoomedCard;
+	
+	public DrawableCard(Card card, JPanel parent){
+		super(new ImageIcon(card.getLilImageLink()));
+		
+		this.setSize(CARD_WIDTH, CARD_HEIGHT);
+		
+		zoomedCard = new ZoomedCard(card);	
+		parent.add(zoomedCard);
+		
+		parent.setComponentZOrder(zoomedCard, 0);
+		
+		this.setBounds(0, 0, CARD_WIDTH, CARD_HEIGHT);
+		
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
+		
+		this.setVisible(true);
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		int x = e.getPoint().x +this.getX() +1;
+		int y = e.getPoint().y +this.getY() +1;
+		
+		zoomedCard.setLocation(x,y);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		zoomedCard.setVisible(true);
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		zoomedCard.setVisible(false);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {}
+}
